dc5094043e19cc0295264a482823cf41c4135784
ljr123123 <1422768678@qq.com>
2024-04-02 21:47:24 +0800

    ...

--- MathematicsPrinciples/1.py
+++ MathematicsPrinciples/1.py
@@ -1,6 +1,4 @@
 # 导入所需的库
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
